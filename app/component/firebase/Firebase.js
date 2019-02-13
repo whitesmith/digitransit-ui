e31a68c959fe8cb1289fe4eb0856df187de04661
Rui Magalhaes <ruip.magalhaes@gmail.com>
2019-02-13 13:58:27 +0000

    added google signin methods

--- app/component/firebase/Firebase.js
+++ app/component/firebase/Firebase.js
@@ -5,8 +5,10 @@ class Firebase {
   constructor(config) {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.googleProvider = new app.auth.GoogleAuthProvider();
   }
 
+  // email methods
   createUserWithEmailAndPassword = (email, password) =>
     this.auth.createUserWithEmailAndPassword(email, password);
 
@@ -18,6 +20,9 @@ class Firebase {
   resetPassword = email => this.auth.sendPasswordResetEmail(email);
 
   updatePassword = password => this.auth.currentUser.updatePassword(password);
+
+  // google login methods
+  signInWithGoogle = () => this.auth.signInWithPopup(this.googleProvider);
 }
 
 export default Firebase;
