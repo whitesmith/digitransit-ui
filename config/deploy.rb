2ccea5a41c4b7759001f855279f5f75a32808c0f
Nuno Correia <nunolopesrb@gmail.com>
2019-02-15 20:10:45 +0000

    add capistrano config

---
+++ config/deploy.rb
@@ -0,0 +1,59 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.1"
+
+set :application, "digitransit-ui"
+set :repo_url, "https://github.com/whitesmith/digitransit-ui.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+  set :deploy_to, "/var/www/digitransit-ui"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, [".env"]
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+
+set :ssh_options, { :forward_agent => true }
+after 'deploy:publishing', 'deploy:restart'
+
+namespace :deploy do
+  desc 'Launch node server'
+
+  task :restart do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      within release_path do
+        execute "ps aux | grep /var/www/digitransit-ui/ |  awk '{print $2}' | xargs -r kill -9 "
+        execute "systemctl stop server"
+        execute "lsof -t -i :8080 | awk '{print $1}' | xargs -r kill -9"
+        execute "cd '#{release_path}'; yarn install; systemctl start server"
+      end
+    end
+  end
+end
