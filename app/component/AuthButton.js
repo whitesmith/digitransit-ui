e3f0c6a101b43977c0a88e335580df491d80cebb
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2019-02-20 12:19:37 +0000

    add dropdown menu to navbar

--- app/component/AuthButton.js
+++ app/component/AuthButton.js
@@ -3,11 +3,42 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { withAuthentication } from './session';
 import { routerShape } from 'react-router';
+import Avatar from 'material-ui/Avatar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+const resetStyle = {color: '', background: 'unset', fontSize: ''};
+const initials = name => name.split(' ').reduce((r, w) => r += w.slice(0, 1), '');
 
 const navAuthButton = (id, textId, defaultMessage, executeAction) => (
   <button id={id} className={`navi-button shy-left`} onClick={executeAction}>
     <FormattedMessage id={textId} defaultMessage={defaultMessage} />
   </button>
+);
+
+const navMenuButton = (id, textId, defaultMessage, executeAction) => (
+  <MenuItem
+    id={id}
+    style={resetStyle}
+    className='navi-menu__item'
+    onClick={executeAction}
+  >
+    <FormattedMessage id={textId} defaultMessage={defaultMessage} />
+  </MenuItem>
+);
+
+const AvatarFallback = (url, name) => (
+  <IconButton className='navi-menu__avatar-container'>
+    <Avatar 
+      style={resetStyle}
+      className='navi-menu__avatar' 
+      alt={initials(name)} 
+      src={ url ? url : null }
+    >
+      { url ? null : initials(name) }
+    </Avatar>
+  </IconButton>
 );
 
 class AuthButton extends React.Component {
@@ -16,14 +47,19 @@ class AuthButton extends React.Component {
     const { router } = this.context;
     if (authUser) {
       return (
-        <>
-          {navAuthButton('profile', 'profile', 'Profile', () => {
+        <IconMenu
+          className='navi-menu shy-left'
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+          targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+          iconButtonElement={AvatarFallback(authUser.photoURL, authUser.displayName)}
+        >
+          {navMenuButton('profile', 'profile', 'Profile', () => {
             router.push('/profile');
           })}
-          {navAuthButton('signout', 'sign-out', 'Sign out', () => {
+          {navMenuButton('signout', 'sign-out', 'Sign out', () => {
             firebase.signOut().then(() => router.push('/'));
           })}
-        </>
+        </IconMenu>
       );
     }
 
