bb9102c878893e623f4dfc2693ebceab6d26bc39
Rui Magalhaes <ruip.magalhaes@gmail.com>
2019-02-26 19:46:39 +0000

    juicy data for the history search UI

--- app/component/AccountHistoryPage.js
+++ app/component/AccountHistoryPage.js
@@ -7,6 +7,8 @@ import withBreakpoint from '../util/withBreakpoint';
 import DeparturesTable from './DeparturesTable';
 import Stat from './Stat';
 import RecentSearchRow from './RecentSearchRow';
+import Loading from './Loading';
+import { withAuthentication } from './session';
 
 class AccountHistoryPage extends React.Component {
   static contextTypes = {
@@ -18,81 +20,63 @@ class AccountHistoryPage extends React.Component {
     breakpoint: PropTypes.string.isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      recentSearches: [],
+      loading: false,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    const { firebase, authUser } = this.props;
+    if (authUser !== prevProps.authUser && authUser) {
+      this.setState({ loading: true });
+      firebase.getUserSearchHistory().then(snap => {
+        const results = [];
+
+        snap.forEach(s => {
+          let search = s.val();
+          const { legs } = search;
+          if (legs && legs.length > 0) {
+            const legsLen = legs.length;
+
+            const distanceComparator = (a, b) => b.distance - a.distance;
+            // get transit legs ordered by distance
+            const orderedTransitLegs = legs
+              .filter(leg => leg.transitLeg)
+              .sort(distanceComparator);
+
+            results.unshift({
+              ...search,
+              duration: legs.reduce((acc, leg) => acc + leg.duration, 0),
+              from: legs[0].from.name,
+              to: legs[legsLen - 1].to.name,
+              // use biggest transit leg route or a custom walk/bike route otherwise
+              via:
+                orderedTransitLegs.length === 0
+                  ? {
+                      color: '187EC2',
+                      mode: legs.sort(distanceComparator)[0].mode,
+                    }
+                  : orderedTransitLegs[0].route,
+            });
+          }
+        });
+        this.setState({ recentSearches: results, loading: false });
+      });
+    }
+  }
+
   render() {
     const { config, router } = this.context;
     const { breakpoint } = this.props;
+    const { recentSearches, loading } = this.state;
     const desktop = breakpoint === 'large';
-    const recentSearches = [
-      {
-        __dataID__: 1,
-        from: 'Aliados',
-        to: 'Hosp. S. João',
-        via: {
-          color: "187EC2",
-          gtfsId: "1:301",
-          longName: "Hosp. S. João",
-          mode: "TRAM",
-          shortName: "301"
-        },
-        duration: 1020,
-        walkingDistance: 500
-      },
-      {
-        __dataID__: 2,
-        from: 'Santa Catarina',
-        to: 'S. Pedro da Costódia',
-        via: {
-          color: "A250FB",
-          gtfsId: "1:801",
-          longName: "Santa Catarina",
-          mode: "BUS",
-          shortName: "801"
-        },
-        duration: 1920,
-        walkingDistance: 260
-      },
-      {
-        __dataID__: 3,
-        from: 'Avenida dos Aliados',
-        to: 'Jardim da Praça',
-        via: {
-          color: "333333",
-          mode: "CAR"
-        },
-        duration: 480
-      },
-      {
-        __dataID__: 4,
-        from: 'Santa Catarina',
-        to: 'Aliados',
-        via: {
-          color: "FF7900",
-          gtfsId: "1:905",
-          longName: "Aliados",
-          mode: "BUS",
-          shortName: "905"
-        },
-        duration: 1260,
-        walkingDistance: 370
-      },
-      {
-        __dataID__: 5,
-        from: 'Aliados',
-        to: 'Rua de Fernandes',
-        via: {
-          color: "6ba3af",
-          mode: "WALK"
-        },
-        duration: 300,
-        walkingDistance: 350
-      },
-    ];
 
     return (
       <div className={`flex-vertical fullscreen bp-${breakpoint}`}>
-        <div
-          className="account-history-container"
-        >
+        <div className="account-history-container">
           {/* start usage metrics section */}
           <div className="page-frame fullscreen fullwidth">
             <h2 className="account-heading">
@@ -108,44 +92,48 @@ class AccountHistoryPage extends React.Component {
 
             <div className="stats-container">
               <div className="stat">
-                <Stat 
-                  icon="car-withoutBox" 
-                  textId={"car-emissions"}
-                  defaultMessage={"Car emissions"}
-                  amount={383.2} 
-                  unit={(<>kgCO<sub>2</sub></>)} 
-                  percentage={20} 
+                <Stat
+                  icon="car-withoutBox"
+                  textId={'car-emissions'}
+                  defaultMessage={'Car emissions'}
+                  amount={383.2}
+                  unit={
+                    <>
+                      kgCO<sub>2</sub>
+                    </>
+                  }
+                  percentage={20}
                   inverted={true}
                 />
               </div>
               <div className="stat">
-                <Stat 
-                  icon="public_transport" 
-                  textId={"public-transport"}
-                  defaultMessage={"Public transport"}
-                  amount={285} 
+                <Stat
+                  icon="public_transport"
+                  textId={'public-transport'}
+                  defaultMessage={'Public transport'}
+                  amount={285}
                   unit="km"
-                  percentage={15} 
+                  percentage={15}
                 />
               </div>
               <div className="stat">
-                <Stat 
-                  icon="walk" 
-                  textId={"walking-distance"}
-                  defaultMessage={"Walking distance"}
-                  amount={112.5} 
+                <Stat
+                  icon="walk"
+                  textId={'walking-distance'}
+                  defaultMessage={'Walking distance'}
+                  amount={112.5}
                   unit="km"
-                  percentage={31} 
+                  percentage={31}
                 />
               </div>
               <div className="stat">
-                <Stat 
-                  icon="walk" 
-                  textId={"calories-walked"}
-                  defaultMessage={"Calories walked"}
-                  amount={11.7} 
+                <Stat
+                  icon="walk"
+                  textId={'calories-walked'}
+                  defaultMessage={'Calories walked'}
+                  amount={11.7}
                   unit="kcal"
-                  percentage={-18} 
+                  percentage={-18}
                 />
               </div>
             </div>
@@ -177,23 +165,29 @@ class AccountHistoryPage extends React.Component {
                 </div>
               </li>
             </ul>
-            <div className={`fullscreen recent-searches-table-container ${desktop ? 'frontpage-panel' : ''}`}>
+            <div
+              className={`fullscreen recent-searches-table-container ${
+                desktop ? 'frontpage-panel' : ''
+              }`}
+            >
               {/* start recent searches section */}
-              <DeparturesTable
-                headers={[
-                  { id: 'origin', defaultMessage: 'Origin' },
-                  { id: 'destination', defaultMessage: 'Destination' },
-                  { id: 'via', defaultMessage: 'Via' },
-                  { id: 'duration', defaultMessage: 'Duration' },
-                  { id: 'walk', defaultMessage: 'Walking' },
-                ]}
-                content={recentSearches.map( s => (
-                  <RecentSearchRow 
-                    search={s} 
-                    key={s.__dataID__}
-                  />
-                ))}
-              />
+              {loading ? (
+                <Loading />
+              ) : (
+                <DeparturesTable
+                  headers={[
+                    { id: 'origin', defaultMessage: 'Origin' },
+                    { id: 'destination', defaultMessage: 'Destination' },
+                    { id: 'via', defaultMessage: 'Via' },
+                    { id: 'duration', defaultMessage: 'Duration' },
+                    { id: 'walk', defaultMessage: 'Walking' },
+                  ]}
+                  content={recentSearches.map(s => (
+                    <RecentSearchRow search={s} key={s.__dataID__} />
+                  ))}
+                />
+              )}
+
               {/* end recent searches section */}
             </div>
           </div>
@@ -203,4 +197,4 @@ class AccountHistoryPage extends React.Component {
   }
 }
 
-export default withBreakpoint(AccountHistoryPage);
+export default withAuthentication(withBreakpoint(AccountHistoryPage));
--- app/component/RecentSearchRow.js
+++ app/component/RecentSearchRow.js
@@ -3,45 +3,38 @@ import RouteNumberContainer from './RouteNumberContainer';
 import WalkDistance from './WalkDistance';
 import Duration from './Duration';
 
-const RecentSearchRow = ({search}, context) => {
+const RecentSearchRow = ({ search }, context) => {
   return (
     <tr
       className="next-departure-row-tr recent-search-row"
-      onClick={() => console.log("go to search")}
+      onClick={() => console.log('go to search')}
       style={{ cursor: 'pointer' }}
     >
       <td className="td-origin td-destination">
         <span className="route-destination">
-          <span className="destination">
-            {search.from}
-          </span>
+          <span className="destination">{search.from}</span>
         </span>
       </td>
       <td className="td-destination">
         <span className="route-destination">
-          <span className="destination">
-            {search.to}
-          </span>
+          <span className="destination">{search.to}</span>
         </span>
       </td>
       <td className="td-route-number">
-        <RouteNumberContainer
-          route={search.via}
-        />
+        <RouteNumberContainer route={search.via} />
       </td>
       <td className="td-duration">
         <Duration duration={search.duration} />
       </td>
       <td className="td-distance">
-        {
-          search.walkingDistance ?
-            (<WalkDistance walkDistance={search.walkingDistance} />)
-          :
-            '-'
-        }
+        {search.walkDistance ? (
+          <WalkDistance walkDistance={search.walkDistance} />
+        ) : (
+          '-'
+        )}
       </td>
     </tr>
   );
-}
+};
 
-export default RecentSearchRow
+export default RecentSearchRow;
